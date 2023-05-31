612c1ca8c1b9b321a1890370c8a8ef6e9cf8a82e
thimiramanujaya <128280023+thimiramanujaya@users.noreply.github.com>
2023-05-31 17:41:28 +0530

    initialized new app build

---
+++ app/src/main/java/com/thimanucreations/batterycure/MainActivity.java
@@ -0,0 +1,17 @@
+package com.thimanucreations.batterycure;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      
+    }
+
+}
